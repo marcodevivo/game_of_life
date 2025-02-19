627fb77542fd40e954de8ed8c9a8053e308bdd9d
marcodevivo <marco.devivo@democom.it>
2025-02-19 22:27:36 +0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
+  config.action_cable.url = "wss://game-of-life-u3x7.onrender.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://game-of-life-u3x7.onrender.com" ]
+
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
